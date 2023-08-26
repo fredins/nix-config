c2e84cd1b0f048985789e0306c1c20b4a2367943
Martin Fredin <fredin.martin@gmail.com>
2023-08-26 22:49:33 +0200

    Move files

---
+++ configuration.nix
@@ -0,0 +1,175 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+
+{ inputs, lib, config, pkgs, ... }: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # inputs.cornelis.overlays.cornelis
+
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    # gcc
+  ];
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "modesetting" ];
+    desktopManager.lxqt.enable = true;
+    displayManager = {
+      sddm = {
+        enable = true;
+	      theme = "maldives";
+        #setupScript = 
+	      #''
+	      #feh --bg-scale /home/fm/Pictures/001.jpg
+        #'';
+      };
+      defaultSession = "lxqt";
+      autoLogin = {
+        enable = true;
+	      user = "fm";
+      };
+    };  
+  };
+
+  services.xserver.layout = "se";
+
+  services.unclutter-xfixes = {
+    enable = true;
+    timeout = 3;
+    extraOptions = ["noevent"];
+  };
+  services.clipmenu.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable =  true;
+
+  # Audio
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  environment.sessionVariables = rec {
+    EDITOR = "nvim";
+	  TERMINAL = "alacritty";
+  };
+
+  environment.etc."current-system-packages".text =
+  let
+    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+    sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.unique packages);
+    formatted = builtins.concatStringsSep "\n" sortedUnique;
+  in
+  formatted;
+
+  time.timeZone = "Europe/Stockholm";
+  networking.hostName = "herring";
+  networking.networkmanager.enable = true;
+  programs.nm-applet.enable = true;
+
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.utf8";
+    LC_IDENTIFICATION = "sv_SE.utf8";
+    LC_MEASUREMENT = "sv_SE.utf8";
+    LC_MONETARY = "sv_SE.utf8";
+    LC_NAME = "sv_SE.utf8";
+    LC_NUMERIC = "sv_SE.utf8";
+    LC_PAPER = "sv_SE.utf8";
+    LC_TELEPHONE = "sv_SE.utf8";
+    LC_TIME = "sv_SE.utf8";
+  };
+
+
+  console.keyMap = "sv-latin1";
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot/efi";
+  };
+
+  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    fm = {
+      # TODO: You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      initialPassword = "password";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = [ "wheel" "networkmanager" "Eduroam" ];
+    };
+  };
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  # services.openssh = {
+    # enable = true;
+    # Forbid root login through SSH.
+    # permitRootLogin = "no";
+    # Use keys only. Remove if you want to SSH using password (not recommended)
+    # passwordAuthentication = false;
+  # };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "22.05"; 
+}
---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # TODO: Add any other flake you might need
+    hardware.url = "github:nixos/nixos-hardware";
+
+    cornelis.url = "github:isovector/cornelis";
+    cornelis.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = 
+    { nixpkgs
+    , home-manager
+    , cornelis
+    , ... 
+    }@inputs: {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      herring = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main nixos configuration file <
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "fm@herring" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main home-manager configuration file <
+        modules = [ ./home/home.nix ];
+      };
+    };
+  };
+}
