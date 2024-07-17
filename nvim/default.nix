cca8d749141fd093eac02ca2754aa4c13b8a83da
Martin Fredin <martin.fredin@arm.com>
2024-07-17 10:21:07 +0200

    nvim: Use 4 spaces

--- nvim/default.nix
+++ nvim/default.nix
@@ -22,9 +22,9 @@
     extraLuaConfig = '' 
       vim.g.mapleader     = " "
       vim.opt.incsearch   = true
-      vim.opt.tabstop     = 2
-      vim.opt.softtabstop = 2
-      vim.opt.shiftwidth  = 2
+      vim.opt.tabstop     = 4
+      vim.opt.softtabstop = 4
+      vim.opt.shiftwidth  = 4
       vim.opt.expandtab   = true
       vim.opt.backup      = false
       vim.opt.scrolloff   = 10
