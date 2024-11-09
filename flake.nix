acefaedc6690cb0a06ab96b9f7f2d92b9d7b9168
Martin Fredin <fredin.martin@gmail.com>
2024-11-09 13:41:48 +0100

    Use stable release 24.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Your new nix config";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hardware.url = "github:nixos/nixos-hardware";
